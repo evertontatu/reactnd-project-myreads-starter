a53efe5b3e0172ccef04261d422d9f4a170b48a6
Everton Boni <evertonferreira.de@gmail.com>
2018-07-11 23:17:58 -0300

    add change selected shelf

--- src/App.js
+++ src/App.js
@@ -7,15 +7,28 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
+  constructor(props){
+    super(props);
+    this.state = {books:[]};
   }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) =>{
+      this.setState({books})
+    })
+  }
+
+  componentWillUpdate(){
+    BooksAPI.getAll().then((books) =>{
+      this.setState({books})
+    })
+  }
+
+  changeShelfSelected(book, shelf){
+    BooksAPI.update(book, shelf)
+  }
+  
 
   render() {
     return (
@@ -25,8 +38,11 @@ class BooksApp extends React.Component {
           )}/>
            
           <Route exact path="/" render={() => (
-            <BookList />
-          )}/>       
+            <BookList 
+              books={this.state.books}
+              changeShelf={this.changeShelfSelected}
+            />
+          )}/>
           
       </div>
     )
